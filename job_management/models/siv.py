598b33ea97660057a9c10a24a6f3761cad054555
ahmad <medocolingo@gmail.com>
2019-09-23 11:37:43 +0300

    [enhance siv]

--- job_management/models/siv.py
+++ job_management/models/siv.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo.exceptions import Warning
 
 class JobSIV(models.Model):
     _name='job.siv'
@@ -12,6 +14,9 @@ class JobSIV(models.Model):
     siv_job_id=fields.Many2one('manage.job',string='Job No.')
     job_description=fields.Char(string='Job Description', related='siv_job_id.description')
     siv_line_ids=fields.One2many('siv.line', 'job_siv_id')
+    account_id=fields.Many2one('account.account', string='WIP')
+    location_id=fields.Many2one('stock.location', string='Location')
+    
     @api.model
     def create(self, values):
         res = super(JobSIV, self).create(values)
@@ -25,16 +30,52 @@ class SIVLine(models.Model):
     
     product_id = fields.Many2one('product.product',string='Code', required=True)
     product_inte_ref=fields.Char(string='Description', related='product_id.default_code')
-    quantity=fields.Integer()
+#     quantity=fields.Integer()
+    initial_demand=fields.Float()
+    reserved=fields.Integer(compute='_compute_reserved')
+    done=fields.Integer()
     currency_id = fields.Many2one('res.currency', string="Currency", related='product_id.currency_id')
     standard_price=fields.Float(string='Rate', related='product_id.standard_price')
     amount=fields.Monetary(compute='_compute_amount')
     job_siv_id=fields.Many2one('job.siv')
+    account_id=fields.Many2one('account.account', string='Account')
     
-    @api.depends('quantity','standard_price')
+    @api.depends('initial_demand','standard_price')
     def _compute_amount(self):
         for rec in self:
-            rec.amount=rec.quantity*rec.standard_price
+            rec.amount=rec.initial_demand*rec.standard_price
+            
+    @api.one
+    @api.depends('initial_demand')
+    def _compute_reserved(self):
+        for rec in self:
+            stock_obj=self.env['stock.quant'].search([('product_id.default_code','=',rec.product_id.default_code),('quantity','>',0.0)],limit=1)
+            if stock_obj:
+                result=stock_obj.quantity-(rec.initial_demand/2)
+                stock_obj.write({'quantity':result})
+                self.reserved=self.initial_demand
+#         
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+    
+    
+    
+    
+    
+    
+    
     
     
     
