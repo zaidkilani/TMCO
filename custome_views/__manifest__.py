5670d6942171f1cf35801163c329fdedc3a8e709
Your Name1 <you@example.com1>
2020-01-01 22:50:54 +0200

    [ADD] add custome_views

---
+++ custome_views/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custome Views",
+    'summary': """""",
+
+    'description': """
+    """,
+
+    'author': "Codesk Company",
+    'website': "http:www.codesk.systems",
+
+    'category': 'Uncategorized',
+    'version': '12.0.1.0',
+    'depends': ['base','stock_account'],
+
+    'data': ['views/views.xml'
+    ],
+}
